31f102edbe46c2e7da24fce2385c01959378b49b
김형민 <kimtuti@naver.com>
2024-03-14 14:04:42 +0900

    [FE-Component] Add : 폰트 설정 (#8)

--- app/frontend/src/config/Color.ts
+++ app/frontend/src/config/Color.ts
@@ -3,7 +3,7 @@
 * 피그마 컬러팔레트 프레임 명
 */
 
-export const MAINCOLER: string = '#FCC434';
+export const MAINYELLOR: string = '#FCC434';
 export const MAINBLACK: string = '#000000';
 export const MAINGRAY: string = '#CCCCCC';
 export const BUTTONSELECT: string = '#261D08';
@@ -12,3 +12,4 @@ export const BLUEBASE: string = '#0BB7FF';
 export const REDBASE: string = '#FF0000';
 export const MAINWHITE: string = '#FFFFFF';
 export const TEXTGRAY: string = '#F2F2F2';
+export const BTNGRAY: string = '#3B3B3B';
---
+++ app/frontend/src/config/Font.ts
@@ -0,0 +1,6 @@
+/*
+* 사용되는 폰트 설정공간
+* 피그마 컬러팔레트 프레임 명
+*/
+
+export const MAINFONT: string = 'Jalnan2TTF';
