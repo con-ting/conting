7264dce9c18c00361c872931b2d783327f4f931f
김형민 <kimtuti@naver.com>
2024-03-15 15:58:02 +0900

    [FE-Component] Add : 드롭다운 컴포넌트 구현 (#12)
    - dropdown 컴포넌트 생성
    - 네비게이션, 추첨결과 페이지에 드롭다운의 간단한 테스트 코드 추가

--- app/frontend/src/screens/lotteryResultScreen/ReservationWaitingScreen.tsx
+++ app/frontend/src/screens/lotteryResultScreen/ReservationWaitingScreen.tsx
@@ -1,15 +1,33 @@
 import React, {useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import { SimpleInput, MultiLineInput } from '../../components/input/input';
+import { Dropdown } from '../../components/dropdown/Dropdown';
+import { widthPercent } from '../../config/Dimensions';
 
 const ReservationWaitingScreen = () => {
     const [empId, setEmpId] = useState('');
+
+    const [dropDownTestOpen, setDropDownTestOpen] = useState(false);
+    const [selectedDropDownTest, setSelectedDropDownTest] = useState(null);
+    const handleSidoItemSelect = (selectedValue) => {
+      setSelectedDropDownTest(selectedValue);
+    };
+    const hospitalData = [
+      { label: '테스트1', value: '테스트벨류' },
+      { label: '테스트2', value: '테스트벨류2' },
+      { label: '테스트3', value: '테스트벨류3' },
+      { label: '테스트4', value: '테스트벨류4' },
+      { label: 'test', value: 'test' },
+    ];
+
     return (
       <View style={styles.container}>
         <Text style={styles.text}>환불대기중 페이지</Text>
         <Text style={styles.text}>메인 페이지</Text>
         <SimpleInput placeholder='하기싫어' value={empId} onChangeText={setEmpId} width='30%' />
         <MultiLineInput placeholder='하기싫어' value={empId} onChangeText={setEmpId} width='70%' height={100} />
+        
+        <Dropdown data={hospitalData} width={widthPercent(165)} placeholder='드롭다운 테스트' zIndexInverse={10} open={dropDownTestOpen} setOpen={setDropDownTestOpen} onSelectValue={handleSidoItemSelect} textSize={12} />
       </View>
     );
 }
