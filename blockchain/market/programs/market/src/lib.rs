8d1b8485cd0bb9b4366984b4c46c61622ccdc575
Younghun Ko <koyo@koyo.kr>
2024-03-18 16:56:15 +0900

    [BC] Add : NFT 티켓 거래 프로그램 뼈대 구현 (#15)

--- blockchain/market/programs/market/src/lib.rs
+++ blockchain/market/programs/market/src/lib.rs
@@ -6,10 +6,59 @@ declare_id!("8Zg6pznFrTJTLgeT6p5FVhLSwiaMR3xHEGrBLpLQoYpx");
 pub mod market {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_market(ctx: Context<CreateMarket>) -> Result<()> {
+        ctx.accounts.market.tickets = Vec::new();
+        Ok(())
+    }
+
+    pub fn sell(ctx: Context<Sell>, nft_address: Pubkey, lamports_price: u64) -> Result<()> {
+        let ticket = Ticket {
+            nft_address,
+            lamports_price,
+        };
+        ctx.accounts.market.tickets.push(ticket);
+        Ok(())
+    }
+
+    pub fn buy(ctx: Context<Buy>, nft_address: Pubkey) -> Result<()> {
+        ctx.accounts
+            .market
+            .tickets
+            .retain(|item| item.nft_address != nft_address);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateMarket<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub market: Account<'info, Market>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Market {
+    pub tickets: Vec<Ticket>,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone)]
+pub struct Ticket {
+    pub nft_address: Pubkey,
+    pub lamports_price: u64,
+}
+
+#[derive(Accounts)]
+pub struct Sell<'info> {
+    #[account(mut)]
+    pub market: Account<'info, Market>,
+    pub seller: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Buy<'info> {
+    #[account(mut)]
+    pub market: Account<'info, Market>,
+    pub buyer: Signer<'info>,
+}
--- blockchain/market/tests/market.ts
+++ blockchain/market/tests/market.ts
@@ -1,16 +1,57 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { Market } from "../target/types/market";
+import * as anchor from '@coral-xyz/anchor'
+import { Program } from '@coral-xyz/anchor'
+import { Market } from '../target/types/market'
+import { expect } from 'chai'
 
-describe("market", () => {
+describe('market', () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  anchor.setProvider(anchor.AnchorProvider.env())
+  const program = anchor.workspace.Market as Program<Market>
+  const market = anchor.web3.Keypair.generate()
+  const ticket = anchor.web3.Keypair.generate()
+  const seller = anchor.web3.Keypair.generate()
+  const buyer = anchor.web3.Keypair.generate()
 
-  const program = anchor.workspace.Market as Program<Market>;
 
-  it("Is initialized!", async () => {
+  it('Creates a market account', async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+    const tx = await program.methods.createMarket()
+      .accounts({ market: market.publicKey })
+      .signers([market])
+      .rpc()
+    console.log('TxHash ::', tx)
+  })
+
+  it('Sells a ticket', async () => {
+    const tx = await program.methods.sell(
+      ticket.publicKey,
+      new anchor.BN(1000000),
+    )
+      .accounts({
+        market: market.publicKey,
+        seller: seller.publicKey,
+      })
+      .signers([seller])
+      .rpc()
+    console.log('TxHash ::', tx)
+  })
+
+  it('Lists tickets for sale', async () => {
+    const marketState = await program.account.market.fetch(market.publicKey)
+    console.log('Tickets for sale ::', marketState.tickets)
+    expect(marketState.tickets.length).to.eq(1)
+  })
+
+  it('Buys a ticket', async () => {
+    const tx = await program.methods.buy(
+      ticket.publicKey,
+    )
+      .accounts({
+        market: market.publicKey,
+        buyer: buyer.publicKey,
+      })
+      .signers([buyer])
+      .rpc()
+    console.log('TxHash ::', tx)
+  })
+})
