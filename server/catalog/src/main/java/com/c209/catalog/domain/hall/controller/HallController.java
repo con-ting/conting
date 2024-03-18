3fabeb52dc47b21e3eb1c6c74519ca2bafd83b06
오유진 <yjin0414@naver.com>
2024-03-18 17:49:55 +0900

    [BE]Tmp: 임시 저장

--- server/catalog/src/main/java/com/c209/catalog/domain/hall/controller/HallController.java
+++ server/catalog/src/main/java/com/c209/catalog/domain/hall/controller/HallController.java
@@ -1,8 +1,11 @@
 package com.c209.catalog.domain.hall.controller;
 
 import com.c209.catalog.domain.hall.dto.HallDto;
+import com.c209.catalog.domain.hall.dto.response.HallsResponse;
 import com.c209.catalog.domain.hall.service.HallService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,8 +22,18 @@ public class HallController {
         this.hallService = hallService;
     }
 
+
+    //<>안에 들어가는 것을 제너릭이라함
+    //제너릭 : 객체 안에 들어가는 타입을 명시
+    //컨트롤러에서는 ResponseEntity를 반환한다. -> 프론트에게 http status를 같이 줘야하기 때문!
+
+
+
     @GetMapping
-    public List<HallDto> getHallResponse(@RequestParam("keyword") String keyword, @RequestParam("region") String region) {
-        return hallService.findHallsByKeywordAndRegion(keyword, region);
+    public ResponseEntity<HallsResponse> getHallResponse(@RequestParam("keyword") String keyword, @RequestParam("region") String region) {
+
+//       흔한 오류들은 여기에서 적기도 함(handler 따로 안하고. 바디를 직접 커스텀하고싶을 경우에만 아래 주석처럼 사용
+        //ResponseEntity.status(HttpStatus.ACCEPTED).body(hallService.findHallsByKeywordAndRegion(keyword, region));
+        return ResponseEntity.ok(hallService.findHallsByKeywordAndRegion(keyword, region));
     }
 }
--- server/catalog/src/main/java/com/c209/catalog/domain/hall/service/impl/HallServiceImpl.java
+++ server/catalog/src/main/java/com/c209/catalog/domain/hall/service/impl/HallServiceImpl.java
@@ -1,6 +1,8 @@
+// HallServiceImpl.java
 package com.c209.catalog.domain.hall.service.impl;
 
 import com.c209.catalog.domain.hall.dto.HallDto;
+import com.c209.catalog.domain.hall.dto.response.HallsResponse;
 import com.c209.catalog.domain.hall.entity.Hall;
 import com.c209.catalog.domain.hall.exception.HallErrorCode;
 import com.c209.catalog.domain.hall.repository.HallRepository;
@@ -10,7 +12,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,18 +25,39 @@ public class HallServiceImpl implements HallService {
     private final HallRepository hallRepository;
 
     @Override
-    public List<HallDto> findHallsByKeywordAndRegion(String keyword, String region) {
+    public HallsResponse findHallsByKeywordAndRegion(String keyword, String region) {
         List<Hall> halls = hallRepository.findByNameContainingAndAddressContaining(keyword, region);
+        List<HallDto> hallDtos = new ArrayList<>();
+        for(Hall h: halls){
+            hallDtos.add(HallDto
+                    .builder()
+                    .id(h.getId())
+                    .name((h.getName()))
+                    .build());
+        }
+
+//        stream, for문 사용하는데, stream 권장(많이 사용)
+//        stream이 상대적으로 어려워서 for문 사용해도 큰 문제는 없을듯(이번 프로젝트)
+//        항상 빌더 쓸 때,  빌더-빌드 세트로 있다는거 유의하자
+//        필요한 것들 모두 빌더 안에 적어주어야 한다. (스트림, 포문 둘 다)
+//        for문 사용할 때에는 halls에 있는 것들 반복하면서 리스트에 담아줌.
+
+
+
+
+
 
         if (halls.isEmpty()) throw new HallException(HallErrorCode.NOT_EXIST_HALL);
 
-        return getHallDto(halls);
+        return HallsResponse.builder().halls(hallDtos).build();
     }
 
-    private List<HallDto> getHallDto(List<Hall> hallList) {
-        return hallList.stream()
+    private HallsResponse groupHallsByKey(List<Hall> hallList) {
+        Map<String, List<HallDto>> resultMap = new HashMap<>();
+        resultMap.put("halls", hallList.stream()
                 .map(this::convertToDto)
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
+        return resultMap;
     }
 
     private HallDto convertToDto(Hall hall) {
