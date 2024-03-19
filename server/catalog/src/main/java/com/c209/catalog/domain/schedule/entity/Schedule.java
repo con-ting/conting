835e087c58178c1deb18549736ba2dd5cf5a4a10
오유진 <yjin0414@naver.com>
2024-03-19 15:41:41 +0900

    [BE]Add: 공연일정 엔터티 추가

--- server/catalog/src/main/java/com/c209/catalog/domain/schedule/entity/Schedule.java
+++ server/catalog/src/main/java/com/c209/catalog/domain/schedule/entity/Schedule.java
@@ -3,11 +3,20 @@ package com.c209.catalog.domain.schedule.entity;
 import com.c209.catalog.domain.show.entity.Show;
 import com.c209.catalog.global.common.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Entity
 public class Schedule extends BaseEntity {
     @GeneratedValue(strategy=IDENTITY)
     @Column(name="schedule_id")
