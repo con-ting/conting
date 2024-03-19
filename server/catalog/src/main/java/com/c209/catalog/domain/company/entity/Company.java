8cd349980b645ec40967be1b5f924b474561d321
오유진 <yjin0414@naver.com>
2024-03-19 13:51:53 +0900

    [BE]Add: showì±주최사 ° 추가

--- server/catalog/src/main/java/com/c209/catalog/domain/company/entity/Company.java
+++ server/catalog/src/main/java/com/c209/catalog/domain/company/entity/Company.java
@@ -1,12 +1,13 @@
 package com.c209.catalog.domain.company.entity;
 
+import com.c209.catalog.global.common.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
-public class Company {
+public class Company extends BaseEntity {
     @GeneratedValue(strategy=IDENTITY)
     @Column(name="company_id")
     @Id
--- server/catalog/src/main/java/com/c209/catalog/domain/show/entity/Show.java
+++ server/catalog/src/main/java/com/c209/catalog/domain/show/entity/Show.java
@@ -1,4 +1,14 @@
 package com.c209.catalog.domain.show.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
 public class Show {
+    @GeneratedValue(strategy=IDENTITY)
+    @Column(name="company_id")
+    @Id
+    private Long id;
 }
