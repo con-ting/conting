c8547a4e49bcac9eaa8447f4f187285d152ea395
오유진 <yjin0414@naver.com>
2024-03-19 13:50:29 +0900

    [BE]Add: showì±c¬ 폴ë E엔터티 추가

--- server/catalog/src/main/java/com/c209/catalog/domain/company/entity/Company.java
+++ server/catalog/src/main/java/com/c209/catalog/domain/company/entity/Company.java
@@ -1,4 +1,20 @@
 package com.c209.catalog.domain.company.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
 public class Company {
+    @GeneratedValue(strategy=IDENTITY)
+    @Column(name="company_id")
+    @Id
+    private Long id;
+
+    @Column
+    private String company_name;
+
+    @Column
+    private String call;
 }
