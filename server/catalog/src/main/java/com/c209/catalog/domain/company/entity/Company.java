a2381c9fdfad97b84db5e6a70d2034a30e03587b
오유진 <yjin0414@naver.com>
2024-03-19 15:41:11 +0900

    [BE]Add: 공연일정 엔터티 추가

--- server/catalog/src/main/java/com/c209/catalog/domain/company/entity/Company.java
+++ server/catalog/src/main/java/com/c209/catalog/domain/company/entity/Company.java
@@ -2,11 +2,21 @@ package com.c209.catalog.domain.company.entity;
 
 import com.c209.catalog.global.common.BaseEntity;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Entity
 public class Company extends BaseEntity {
     @GeneratedValue(strategy=IDENTITY)
     @Column(name = "company_id")
