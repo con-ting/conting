d8406d580ec693b5a720f61bd8c85573136372f6
오유진 <yjin0414@naver.com>
2024-03-20 14:37:19 +0900

    [BE]Add: 공연컨트롤러 추가

--- server/catalog/src/main/java/com/c209/catalog/domain/performance/controller/PerformanceController.java
+++ server/catalog/src/main/java/com/c209/catalog/domain/performance/controller/PerformanceController.java
@@ -1,4 +1,23 @@
 package com.c209.catalog.domain.performance.controller;
 
+import com.c209.catalog.domain.performance.service.PerformanceService;
+import com.c209.catalog.domain.singer.dto.response.GetSingerResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/show")
+@RequiredArgsConstructor
 public class PerformanceController {
+//    private final PerformanceService performanceService;
+
+    @GetMapping("{show_id}")
+    public ResponseEntity<GetSingerResponse> getSingerDetail(
+//            @RequestHeader("X-Authorization-Id") Long memberId,
+            @PathVariable("show_id") Long showId
+    ){
+        return null;
+//        return ResponseEntity.ok(performanceService.getShowDetails(showId));
+    }
 }
