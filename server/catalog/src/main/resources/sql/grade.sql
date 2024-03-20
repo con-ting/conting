6440c1ed22862b021f8d8b9e9bf03d6fe49b58c3
오유진 <yjin0414@naver.com>
2024-03-20 11:15:01 +0900

    [BE]Add: 좌석등급 SQL 추가

---
+++ server/catalog/src/main/resources/sql/grade.sql
@@ -0,0 +1,11 @@
+ALTER TABLE grade
+    MODIFY COLUMN created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    MODIFY COLUMN updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
+
+INSERT INTO grade (show_id, grade, price)
+    VALUES (1, 'R', 150000),
+           (1, 'S', 120000),
+           (1, 'A', 100000),
+           (2, 'R', 50000),
+           (2, 'S', 30000),
+           (2, 'A', 20000);
