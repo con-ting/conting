91dc61472aeccafce638992ecb3fb2df310b0bae
오유진 <yjin0414@naver.com>
2024-03-20 12:06:40 +0900

    [BE]Refactor: Show->Performance 이름 수정

--- server/catalog/src/main/resources/sql/grade.sql
+++ server/catalog/src/main/resources/sql/grade.sql
@@ -2,7 +2,7 @@ ALTER TABLE grade
     MODIFY COLUMN created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     MODIFY COLUMN updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
 
-INSERT INTO grade (show_id, grade, price)
+INSERT INTO grade (performance_id, grade, price)
     VALUES (1, 'R', 150000),
            (1, 'S', 120000),
            (1, 'A', 100000),
