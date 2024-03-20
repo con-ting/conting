b22b275613e2f71bc5d1a4c9577b993f9d12cf5d
전상혁 <tkdgur0826@gmail.com>
2024-03-20 13:59:29 +0900

    [FE-Component] Add : 결제하기 백 헤더 구현 (#5)

--- app/frontend/src/components/seat/SeatSum.tsx
+++ app/frontend/src/components/seat/SeatSum.tsx
@@ -2,6 +2,7 @@ import {StyleSheet, Text, View} from 'react-native';
 import seatsData from '../data/seatsData';
 import {F_SIZE_TITLE, F_SIZE_Y_HEADER} from '../../config/Font';
 import {YellowButton} from '../button/Button';
+import {useNavigation} from '@react-navigation/native';
 
 type SeatSumProps = {
   selectedSeats: Array<string>;
@@ -12,6 +13,8 @@ type SeatSumProps = {
 };
 
 export default function SeatSum(props: SeatSumProps) {
+  const navigation = useNavigation();
+
   const calTotalPrice = () => {
     return props.selectedSeats.reduce((total, seatId) => {
       const seat = props.seatsData.find(s => s.id === seatId);
@@ -24,13 +27,28 @@ export default function SeatSum(props: SeatSumProps) {
     return null;
   }
 
+  const selectedSeatsInfo = props.selectedSeats
+    .map(seatId => {
+      const seat = props.seatsData.find(s => s.id === seatId);
+      return seat ? {seat_id: seat.id, price: seat.price} : null;
+    })
+    .filter(info => info !== null); // 필터링하여 null 제거
+
   return (
     <View style={styles.container}>
       <View>
         <Text style={F_SIZE_TITLE}>합계</Text>
         <Text style={F_SIZE_Y_HEADER}>{calTotalPrice()} 원</Text>
       </View>
-      <YellowButton width={190} btnText="티켓 구매" textSize={20} isRadius />
+      <YellowButton
+        onPress={() =>
+          navigation.navigate('Pay', {selectedSeats: selectedSeatsInfo})
+        }
+        width={190}
+        btnText="티켓 구매"
+        textSize={20}
+        isRadius
+      />
     </View>
   );
 }
--- app/frontend/src/navigation/BottomTabNavigator.tsx
+++ app/frontend/src/navigation/BottomTabNavigator.tsx
@@ -24,6 +24,7 @@ import {
 import WaitingScreen from '../screens/mainScreen/ticketingScreen/WaitingScreen';
 import SeatingAreaSelectScreen from '../screens/mainScreen/ticketingScreen/SeatingAreaSelectScreen';
 import {heightPercent} from '../config/Dimensions';
+import PaymentScreen from '../screens/mainScreen/ticketingScreen/PaymentScreen';
 
 const BottomTab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -263,6 +264,27 @@ export default function AppContainer() {
             headerShown: true,
             tabBarVisible: false,
             title: '구역 선택',
+            headerStyle: {
+              backgroundColor: 'black',
+              borderBottomColor: 'transparent', //헤더 테두리 색 제거
+              elevation: 0, //Android에서 헤더 그림자 제거
+              height: heightPercent(60),
+            },
+            headerTintColor: '#FFFFFF',
+            headerTitleStyle: {
+              fontFamily: 'Jalnan2TTF',
+            },
+            headerTitleAlign: 'center',
+            // 개별 스크린에 이펙트 적용 (현재 대기열 진입시 오른쪽에서 왼쪽으로 페이지 전환)
+            cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+          }}></Stack.Screen>
+        <Stack.Screen
+          name="Pay"
+          component={PaymentScreen}
+          options={{
+            headerShown: true,
+            tabBarVisible: false,
+            title: '결제하기',
             headerStyle: {
               backgroundColor: 'black',
               borderBottomColor: 'transparent', //헤더 테두리 색 제거
