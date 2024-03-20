73abbb48e7f12659149470a42f7eeabb63dbfae6
오유진 <yjin0414@naver.com>
2024-03-20 14:40:13 +0900

    [BE]Add: 공연에러코드 추가

--- server/catalog/src/main/java/com/c209/catalog/domain/performance/exception/PerformanceErrorCode.java
+++ server/catalog/src/main/java/com/c209/catalog/domain/performance/exception/PerformanceErrorCode.java
@@ -1,4 +1,16 @@
 package com.c209.catalog.domain.performance.exception;
 
-public class PerformanceErrorCode {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@Getter
+@AllArgsConstructor
+public enum PerformanceErrorCode {
+    NOT_EXIST_SHOW("공연을 조회할 수 없습니다.", BAD_REQUEST);
+
+    private final String message;
+    private final HttpStatus httpStatus;
 }
