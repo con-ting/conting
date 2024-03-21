df36399a466851d6823c9a7f009b39c588f03e08
오유진 <yjin0414@naver.com>
2024-03-21 15:26:03 +0900

    [BE]Add: 조회수 ì¶ 추가

--- server/catalog/src/main/java/com/c209/catalog/domain/singer/controller/SingerController.java
+++ server/catalog/src/main/java/com/c209/catalog/domain/singer/controller/SingerController.java
@@ -3,7 +3,9 @@ package com.c209.catalog.domain.singer.controller;
 
 
 import com.c209.catalog.domain.singer.dto.response.GetSingerResponse;
+import com.c209.catalog.domain.singer.entity.Singer;
 import com.c209.catalog.domain.singer.service.SingerService;
+import com.c209.catalog.domain.singer.service.impl.SingerServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,9 +19,13 @@ public class SingerController {
 
     @GetMapping("{singer_id}")
     public ResponseEntity<GetSingerResponse> getSingerDetail(
-            @RequestHeader("X-Authorization-Id") Long memberId,
+//            @RequestHeader("X-Authorization-Id") Long memberId,
             @PathVariable("singer_id") Long singerId
     ){
+//        Singer singer = singerService.getSingerDetails(singerId);
+//        singer.setView(singer.getView() + 1); // Incrementing the view count
+//        singerService.saveSinger(singer); // Assuming you have a method to save the Singer in your service
+//        return ResponseEntity.ok(singer);
         return ResponseEntity.ok(singerService.getSingerDetails(singerId));
     }
 
