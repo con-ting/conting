6add274c25ee2ffb651498da0c0c30f22ab7060c
Younghun Ko <koyo@koyo.kr>
2024-03-21 17:54:06 +0900

    [BC] Add : NFT 티켓 거래 프로그램 판매 로직 구현 (#15)

--- blockchain/programs/market/src/lib.rs
+++ blockchain/programs/market/src/lib.rs
@@ -1,64 +1,109 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token;
+use anchor_spl::token::{Token, TokenAccount};
 
-declare_id!("8Zg6pznFrTJTLgeT6p5FVhLSwiaMR3xHEGrBLpLQoYpx");
+declare_id!("9dC52fm1DiZyrZkfG6ZhmTn5PM6ekMENKoAeBxHhjRpA");
 
 #[program]
 pub mod market {
+    use mpl_token_metadata::accounts::Metadata;
+
     use super::*;
 
     pub fn create_market(ctx: Context<CreateMarket>) -> Result<()> {
-        ctx.accounts.market.tickets = Vec::new();
+        ctx.accounts.market.authority = ctx.accounts.server.key();
+        ctx.accounts.market.items = Vec::new();
         Ok(())
     }
 
-    pub fn sell(ctx: Context<Sell>, nft_address: Pubkey, lamports_price: u64) -> Result<()> {
-        let ticket = Ticket {
-            nft_address,
-            lamports_price,
+    pub fn sell_ticket(ctx: Context<SellTicket>, lamports: u64) -> Result<()> {
+        // collection이 server owned여야 함
+        let market = &mut ctx.accounts.market;
+        let collection = &ctx.accounts.collection;
+        require_keys_eq!(collection.owner.key(), market.authority);
+
+        // ticket이 collection verified여야 함
+        let ticket = &ctx.accounts.ticket;
+        let metadata = Metadata::try_from(&ticket.to_account_info())?;
+        let metadata_collection = metadata.collection.unwrap();
+        require_keys_eq!(metadata_collection.key, collection.key());
+        require_eq!(metadata_collection.verified, true);
+
+        let item = Item {
+            ticket: ticket.key(),
+            lamports,
         };
-        ctx.accounts.market.tickets.push(ticket);
+        market.items.push(item);
         Ok(())
     }
 
-    pub fn buy(ctx: Context<Buy>, nft_address: Pubkey) -> Result<()> {
-        ctx.accounts
-            .market
-            .tickets
-            .retain(|item| item.nft_address != nft_address);
+    pub fn buy_ticket(ctx: Context<BuyTicket>) -> Result<()> {
+        let market = &mut ctx.accounts.market;
+        let ticket = &ctx.accounts.ticket;
+        if let Some(item_index) = market
+            .items
+            .iter()
+            .position(|item| item.ticket == ticket.key())
+        {
+            let item = &market.items[item_index];
+            let buyer = &ctx.accounts.buyer;
+
+            // **seller.to_account_info().try_borrow_mut_lamports()? = item.lamports;
+            // **buyer.to_account_info().try_borrow_mut_lamports()? = item.lamports;
+        } else {
+            return Err(ErrorCode::TicketNotFound.into());
+        }
+
+        market
+            .items
+            .retain(|item| item.ticket != ctx.accounts.ticket.key());
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct CreateMarket<'info> {
-    #[account(init, payer = user, space = 9000)]
+    #[account(init, payer = server, space = 8 + 24 + 32 + 40 * 100)]
     pub market: Account<'info, Market>,
     #[account(mut)]
-    pub user: Signer<'info>,
+    pub server: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
 #[account]
 pub struct Market {
-    pub tickets: Vec<Ticket>,
+    pub authority: Pubkey,
+    pub items: Vec<Item>,
 }
 
 #[derive(AnchorSerialize, AnchorDeserialize, Clone)]
-pub struct Ticket {
-    pub nft_address: Pubkey,
-    pub lamports_price: u64,
+pub struct Item {
+    pub ticket: Pubkey,
+    pub lamports: u64,
 }
 
 #[derive(Accounts)]
-pub struct Sell<'info> {
+pub struct SellTicket<'info> {
     #[account(mut)]
     pub market: Account<'info, Market>,
     pub seller: Signer<'info>,
+    pub collection: Account<'info, TokenAccount>, // owned by server
+    pub ticket: Account<'info, TokenAccount>,     // owned by seller
+    #[account(address = token::ID)]
+    pub token_program: Program<'info, Token>,
 }
 
 #[derive(Accounts)]
-pub struct Buy<'info> {
+pub struct BuyTicket<'info> {
     #[account(mut)]
     pub market: Account<'info, Market>,
     pub buyer: Signer<'info>,
+    pub ticket: Account<'info, TokenAccount>,
+    pub token_program: Program<'info, Token>,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Ticket not found")]
+    TicketNotFound,
 }
--- blockchain/tests/market.ts
+++ blockchain/tests/market.ts
@@ -5,50 +5,54 @@ import { expect } from 'chai'
 
 describe('market', () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env())
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider)
   const program = anchor.workspace.Market as Program<Market>
-  const market = anchor.web3.Keypair.generate()
-  const ticket = anchor.web3.Keypair.generate()
-  const seller = anchor.web3.Keypair.generate()
-  const buyer = anchor.web3.Keypair.generate()
 
+  const market = anchor.web3.Keypair.generate()
+  const sellerSecret = new Uint8Array([2, 159, 38, 178, 107, 22, 241, 30, 221, 81, 232, 19, 47, 183, 90, 205, 194, 216, 174, 68, 39, 137, 58, 163, 56, 205, 122, 161, 237, 222, 58, 194, 112, 116, 181, 185, 249, 21, 0, 196, 104, 99, 202, 219, 94, 194, 207, 157, 219, 110, 5, 236, 208, 103, 62, 10, 64, 232, 176, 24, 75, 52, 65, 92])
+  const buyerSecret = new Uint8Array([73, 143, 92, 2, 59, 15, 51, 126, 75, 55, 110, 95, 56, 28, 52, 102, 243, 7, 167, 149, 33, 81, 212, 208, 184, 153, 172, 4, 65, 121, 232, 110, 26, 225, 94, 248, 166, 222, 52, 178, 119, 20, 45, 43, 180, 71, 159, 163, 145, 46, 166, 192, 95, 192, 100, 128, 149, 186, 15, 136, 18, 86, 57, 128])
+  const seller = anchor.web3.Keypair.fromSecretKey(sellerSecret)
+  const buyer = anchor.web3.Keypair.fromSecretKey(buyerSecret)
+
+  const collection = new anchor.web3.PublicKey('GFktYvJLGkh5jfdv9ZFuzcXzgNu8bKLhsYnX3DsRU2L6')
+  const ticket = new anchor.web3.PublicKey('5FD27ojtnUrbLg1D2aErsoYQEMUvMXDiyFDtw25TZg5d')
 
   it('Creates a market account', async () => {
-    // Add your test here.
     const tx = await program.methods.createMarket()
-      .accounts({ market: market.publicKey })
+      .accounts({
+        market: market.publicKey
+      })
       .signers([market])
       .rpc()
     console.log('TxHash ::', tx)
   })
 
   it('Sells a ticket', async () => {
-    const tx = await program.methods.sell(
-      ticket.publicKey,
-      new anchor.BN(1000000),
+    const tx = await program.methods.sellTicket(
+      new anchor.BN(anchor.web3.LAMPORTS_PER_SOL),
     )
       .accounts({
         market: market.publicKey,
         seller: seller.publicKey,
+        collection,
+        ticket,
       })
       .signers([seller])
       .rpc()
     console.log('TxHash ::', tx)
-  })
 
-  it('Lists tickets for sale', async () => {
     const marketState = await program.account.market.fetch(market.publicKey)
-    console.log('Tickets for sale ::', marketState.tickets)
-    expect(marketState.tickets.length).to.eq(1)
+    expect(marketState.items.length).to.eq(1)
+    console.log('Tickets for sale ::', marketState.items)
   })
 
   it('Buys a ticket', async () => {
-    const tx = await program.methods.buy(
-      ticket.publicKey,
-    )
+    const tx = await program.methods.buyTicket()
       .accounts({
         market: market.publicKey,
         buyer: buyer.publicKey,
+        ticket,
       })
       .signers([buyer])
       .rpc()
