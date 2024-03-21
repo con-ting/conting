07fa572f3255c80565d6356196de87370b816d9b
오유진 <yjin0414@naver.com>
2024-03-21 16:28:46 +0900

    [BE]Add: 메인페이지 DTO 추가

---
+++ server/catalog/src/main/java/com/c209/catalog/domain/performance/dto/FShowsDto.java
@@ -0,0 +1,34 @@
+package com.c209.catalog.domain.performance.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Data
+public class FShowsDto {
+    private Long show_id;
+    @NotNull
+    private String poster;
+    @NotNull
+    private String title;
+    @NotNull
+    private String hall;
+    @NotNull
+    private String reservation_type;
+    @FutureOrPresent
+    private LocalDateTime reservation_start_date_time;
+    private LocalDateTime reservation_end_date_time;
+    private Date start_date;
+    private Date end_date;
+}
---
+++ server/catalog/src/main/java/com/c209/catalog/domain/performance/dto/PShowsDto.java
@@ -0,0 +1,35 @@
+package com.c209.catalog.domain.performance.dto;
+
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Data
+public class PShowsDto {
+    private Long show_id;
+    @NotNull
+    private String poster;
+    @NotNull
+    private String title;
+    @NotNull
+    private String hall;
+    @NotNull
+    private String reservation_type;
+    private LocalDateTime reservation_start_date_time;
+    private LocalDateTime reservation_end_date_time;
+    private Date start_date;
+    @FutureOrPresent
+    private Date end_date;
+}
