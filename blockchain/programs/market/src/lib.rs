5f2d714de0a43e770c09b9f44deed201d2b30667
Younghun Ko <koyo@koyo.kr>
2024-03-22 03:12:11 +0900

    [BC] Add : NFT 티켓 판매 구현 (#15)

--- blockchain/programs/market/src/lib.rs
+++ blockchain/programs/market/src/lib.rs
@@ -1,36 +1,54 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token;
-use anchor_spl::token::{Token, TokenAccount};
+use anchor_spl::token::{self, Mint, Token, TokenAccount, Transfer};
+use mpl_token_metadata::accounts::Metadata;
+
+const SERVER_PUBKEY: Pubkey = Pubkey::new_from_array([
+    136, 5, 219, 160, 32, 90, 40, 48, 191, 238, 134, 32, 24, 42, 140, 100, 90, 234, 161, 150, 187,
+    81, 89, 3, 188, 143, 164, 145, 14, 44, 167, 74,
+]);
 
 declare_id!("9dC52fm1DiZyrZkfG6ZhmTn5PM6ekMENKoAeBxHhjRpA");
 
 #[program]
 pub mod market {
-    use mpl_token_metadata::accounts::Metadata;
-
     use super::*;
 
     pub fn create_market(ctx: Context<CreateMarket>) -> Result<()> {
-        ctx.accounts.market.authority = ctx.accounts.server.key();
         ctx.accounts.market.items = Vec::new();
         Ok(())
     }
 
     pub fn sell_ticket(ctx: Context<SellTicket>, lamports: u64) -> Result<()> {
-        // collection이 server owned여야 함
-        let market = &mut ctx.accounts.market;
-        let collection = &ctx.accounts.collection;
-        require_keys_eq!(collection.owner.key(), market.authority);
+        // collection은 server owned여야 함
+        let collection_token_account = &ctx.accounts.collection_token_account;
+        require_keys_eq!(collection_token_account.owner.key(), SERVER_PUBKEY);
 
-        // ticket이 collection verified여야 함
-        let ticket = &ctx.accounts.ticket;
-        let metadata = Metadata::try_from(&ticket.to_account_info())?;
+        // ticket은 collection verified여야 함
+        let ticket_metadata_pda = &ctx.accounts.ticket_metadata_pda;
+        let metadata = Metadata::try_from(&ticket_metadata_pda.to_account_info())?;
         let metadata_collection = metadata.collection.unwrap();
-        require_keys_eq!(metadata_collection.key, collection.key());
         require_eq!(metadata_collection.verified, true);
+        require_keys_eq!(metadata_collection.key, collection_token_account.mint);
 
+        // seller의 ticket을 market에게 전송함
+        let ticket_token_account = &ctx.accounts.ticket_token_account;
+        let ticket_token_account_market = &ctx.accounts.ticket_token_account_market;
+        let seller = &ctx.accounts.seller;
+        let token_program = &ctx.accounts.token_program;
+
+        let cpi_accounts = Transfer {
+            from: ticket_token_account.to_account_info(),
+            to: ticket_token_account_market.to_account_info(),
+            authority: seller.to_account_info(),
+        };
+        let cpi_program = token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        token::transfer(cpi_ctx, 1)?;
+
+        let market = &mut ctx.accounts.market;
         let item = Item {
-            ticket: ticket.key(),
+            seller: seller.key(),
+            ticket_mint: ticket_token_account.mint,
             lamports,
         };
         market.items.push(item);
@@ -39,11 +57,12 @@ pub mod market {
 
     pub fn buy_ticket(ctx: Context<BuyTicket>) -> Result<()> {
         let market = &mut ctx.accounts.market;
-        let ticket = &ctx.accounts.ticket;
+        let ticket_mint = &ctx.accounts.ticket_mint;
+
         if let Some(item_index) = market
             .items
             .iter()
-            .position(|item| item.ticket == ticket.key())
+            .position(|item| item.ticket_mint == ticket_mint.key())
         {
             let item = &market.items[item_index];
             let buyer = &ctx.accounts.buyer;
@@ -56,50 +75,67 @@ pub mod market {
 
         market
             .items
-            .retain(|item| item.ticket != ctx.accounts.ticket.key());
+            .retain(|item| item.ticket_mint != ticket_mint.key());
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct CreateMarket<'info> {
-    #[account(init, payer = server, space = 8 + 24 + 32 + 40 * 100)]
-    pub market: Account<'info, Market>,
-    #[account(mut)]
+    #[account(
+        mut,
+        address = SERVER_PUBKEY
+    )]
     pub server: Signer<'info>,
+
+    #[account(
+        init,
+        payer = server,
+        space = 8 + 24 + 72 * 100
+    )]
+    pub market: Account<'info, Market>,
+
     pub system_program: Program<'info, System>,
 }
 
 #[account]
 pub struct Market {
-    pub authority: Pubkey,
     pub items: Vec<Item>,
 }
 
 #[derive(AnchorSerialize, AnchorDeserialize, Clone)]
 pub struct Item {
-    pub ticket: Pubkey,
+    pub seller: Pubkey,
+    pub ticket_mint: Pubkey,
     pub lamports: u64,
 }
 
 #[derive(Accounts)]
 pub struct SellTicket<'info> {
+    pub seller: Signer<'info>,
+
     #[account(mut)]
     pub market: Account<'info, Market>,
-    pub seller: Signer<'info>,
-    pub collection: Account<'info, TokenAccount>, // owned by server
-    pub ticket: Account<'info, TokenAccount>,     // owned by seller
-    #[account(address = token::ID)]
+
+    pub collection_token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub ticket_token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub ticket_token_account_market: Account<'info, TokenAccount>,
+    /// CHECK: validated in access control logic
+    pub ticket_metadata_pda: AccountInfo<'info>,
+
     pub token_program: Program<'info, Token>,
 }
 
 #[derive(Accounts)]
 pub struct BuyTicket<'info> {
+    pub buyer: Signer<'info>,
+
     #[account(mut)]
     pub market: Account<'info, Market>,
-    pub buyer: Signer<'info>,
-    pub ticket: Account<'info, TokenAccount>,
-    pub token_program: Program<'info, Token>,
+
+    pub ticket_mint: Account<'info, Mint>,
 }
 
 #[error_code]
--- blockchain/tests/market.ts
+++ blockchain/tests/market.ts
@@ -1,13 +1,14 @@
 import * as anchor from '@coral-xyz/anchor'
-import { Program } from '@coral-xyz/anchor'
 import { Market } from '../target/types/market'
 import { expect } from 'chai'
+import { createAssociatedTokenAccount, transfer } from '@solana/spl-token'
+import { Metaplex } from '@metaplex-foundation/js'
 
-describe('market', () => {
+describe('market', async () => {
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env()
   anchor.setProvider(provider)
-  const program = anchor.workspace.Market as Program<Market>
+  const program = anchor.workspace.Market as anchor.Program<Market>
 
   const market = anchor.web3.Keypair.generate()
   const sellerSecret = new Uint8Array([2, 159, 38, 178, 107, 22, 241, 30, 221, 81, 232, 19, 47, 183, 90, 205, 194, 216, 174, 68, 39, 137, 58, 163, 56, 205, 122, 161, 237, 222, 58, 194, 112, 116, 181, 185, 249, 21, 0, 196, 104, 99, 202, 219, 94, 194, 207, 157, 219, 110, 5, 236, 208, 103, 62, 10, 64, 232, 176, 24, 75, 52, 65, 92])
@@ -15,8 +16,16 @@ describe('market', () => {
   const seller = anchor.web3.Keypair.fromSecretKey(sellerSecret)
   const buyer = anchor.web3.Keypair.fromSecretKey(buyerSecret)
 
-  const collection = new anchor.web3.PublicKey('GFktYvJLGkh5jfdv9ZFuzcXzgNu8bKLhsYnX3DsRU2L6')
-  const ticket = new anchor.web3.PublicKey('5FD27ojtnUrbLg1D2aErsoYQEMUvMXDiyFDtw25TZg5d')
+  const collectionMint = new anchor.web3.PublicKey('GFktYvJLGkh5jfdv9ZFuzcXzgNu8bKLhsYnX3DsRU2L6')
+  const collectionTokenAccount = new anchor.web3.PublicKey('BEcsABKxv3TCHcYJuhjifTTqAU69bukUQR5pJb87PBjY')
+
+  const ticketMint = new anchor.web3.PublicKey('5FD27ojtnUrbLg1D2aErsoYQEMUvMXDiyFDtw25TZg5d')
+  const ticketTokenAccount = new anchor.web3.PublicKey('FHH4cKJ44UcAQgpQkW5nkjE47rz1vrWhVdXzPAEKxg4r')
+
+  before('airdrop', async () => {
+    await provider.connection.requestAirdrop(seller.publicKey, 1 * anchor.web3.LAMPORTS_PER_SOL)
+    await provider.connection.requestAirdrop(buyer.publicKey, 1 * anchor.web3.LAMPORTS_PER_SOL)
+  })
 
   it('Creates a market account', async () => {
     const tx = await program.methods.createMarket()
@@ -29,14 +38,31 @@ describe('market', () => {
   })
 
   it('Sells a ticket', async () => {
+    // metadata pda
+    const metaplex = Metaplex.make(provider.connection)
+    const ticketMetadataPda = metaplex
+      .nfts()
+      .pdas()
+      .metadata({ mint: ticketMint })
+
+    // ticketTokenAccount of market
+    const ticketTokenAccountMarket = await createAssociatedTokenAccount(
+      provider.connection,
+      seller,
+      ticketMint,
+      market.publicKey,
+    )
+
     const tx = await program.methods.sellTicket(
       new anchor.BN(anchor.web3.LAMPORTS_PER_SOL),
     )
       .accounts({
-        market: market.publicKey,
         seller: seller.publicKey,
-        collection,
-        ticket,
+        market: market.publicKey,
+        collectionTokenAccount,
+        ticketTokenAccount,
+        ticketTokenAccountMarket,
+        ticketMetadataPda,
       })
       .signers([seller])
       .rpc()
@@ -50,12 +76,16 @@ describe('market', () => {
   it('Buys a ticket', async () => {
     const tx = await program.methods.buyTicket()
       .accounts({
-        market: market.publicKey,
         buyer: buyer.publicKey,
-        ticket,
+        market: market.publicKey,
+        ticketMint,
       })
       .signers([buyer])
       .rpc()
     console.log('TxHash ::', tx)
+
+    const marketState = await program.account.market.fetch(market.publicKey)
+    expect(marketState.items.length).to.eq(0)
+    console.log('Tickets for sale ::', marketState.items)
   })
 })
