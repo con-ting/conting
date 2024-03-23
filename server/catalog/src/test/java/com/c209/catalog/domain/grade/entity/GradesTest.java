5aadd0f47585f0b884c095977c926692c5b57dbb
오유진 <yjin0414@naver.com>
2024-03-23 18:56:10 +0900

    [BE]Test: Controller 테스트

--- server/catalog/src/test/java/com/c209/catalog/domain/grade/entity/GradesTest.java
+++ server/catalog/src/test/java/com/c209/catalog/domain/grade/entity/GradesTest.java
@@ -1,25 +1,22 @@
 package com.c209.catalog.domain.grade.entity;
 
+import com.c209.catalog.domain.grade.enums.Grades;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class GradesTest {
     @Test
     @DisplayName("Grade Entity Test")
-
     void createGrade() {
         // given
         Grade.GradeBuilder builder = Grade.builder();
-        builder.grade("S");
+        builder.grade(Grades.S);
         builder.price(10);
         Grade grade = builder.build();
 
         // when, then
-        Assertions.assertThat(grade.getGrade()).isEqualTo("S");
+        Assertions.assertThat(grade.getGrade()).isEqualTo(Grades.S);
         Assertions.assertThat(grade.getPrice()).isEqualTo(10);
     }
-
 }
