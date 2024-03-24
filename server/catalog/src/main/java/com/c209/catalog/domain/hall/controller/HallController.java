c3b30f6d0983de86b3057ba04dcda81f1f8daaa1
오유진 <yjin0414@naver.com>
2024-03-24 23:28:12 +0900

    [BE]Add: param request false로 변경

--- server/catalog/src/main/java/com/c209/catalog/domain/hall/controller/HallController.java
+++ server/catalog/src/main/java/com/c209/catalog/domain/hall/controller/HallController.java
@@ -22,8 +22,10 @@ public class HallController {
 
 
     @GetMapping
-    public ResponseEntity<HallsResponse> getHallResponse(@RequestParam("keyword") String keyword, @RequestParam("region") String region) {
-
+    public ResponseEntity<HallsResponse> getHallResponse(
+            @RequestParam(value = "keyword", required = false, defaultValue = "") String keyword,
+            @RequestParam(value = "region", required = false, defaultValue = "") String region
+    ) {
 //       흔한 오류들은 여기에서 적기도 함(handler 따로 안하고. 바디를 직접 커스텀하고싶을 경우에만 아래 주석처럼 사용
         //ResponseEntity.status(HttpStatus.ACCEPTED).body(hallService.findHallsByKeywordAndRegion(keyword, region));
         return ResponseEntity.ok(hallService.findHallsByKeywordAndRegion(keyword, region));
