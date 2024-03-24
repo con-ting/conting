dd2ad617dc3ad92abb749082a7c7ef3b53484266
Younghun Ko <koyo@koyo.kr>
2024-03-25 06:05:51 +0900

    [BC] Add : NFT 티켓 이벤트 응모 구현 (#16)

--- blockchain/programs/event/src/lib.rs
+++ blockchain/programs/event/src/lib.rs
@@ -1,4 +1,9 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::sysvar::SysvarId;
+use anchor_spl::token::{Mint, TokenAccount};
+use arrayref::array_ref;
+use mpl_token_metadata::accounts::Metadata;
+use mpl_token_metadata::instructions::UseV1CpiBuilder;
 
 const SERVER_PUBKEY: Pubkey = Pubkey::new_from_array([
     136, 5, 219, 160, 32, 90, 40, 48, 191, 238, 134, 32, 24, 42, 140, 100, 90, 234, 161, 150, 187,
@@ -13,42 +18,92 @@ pub mod event {
 
     pub fn create_event(
         ctx: Context<CreateEvent>,
+        start_timestamp: i64,
+        end_timestamp: i64,
+        winners_total: u16,
+        singer_name: String,
         name: String,
         description: String,
-        uri: String,
         goods: String,
-        singer: String,
+        uri: String,
     ) -> Result<()> {
-        let event = &mut ctx.accounts.event;
-        event.name = name;
-        event.description = description;
-        event.uri = uri;
-        event.goods = goods;
-        event.singer = singer;
+        let metadata = Metadata::try_from(&ctx.accounts.collection_metadata_pda.to_account_info())?;
+        let creators = metadata.creators.unwrap();
+        let maybe_server = creators.get(0).unwrap();
+        let maybe_creator = creators.get(1).unwrap();
+        let maybe_singer = creators.get(2).unwrap();
+        require_eq!(maybe_server.verified, true);
+        require_keys_eq!(maybe_server.address, SERVER_PUBKEY);
+        require_keys_eq!(maybe_creator.address, ctx.accounts.creator.key());
+        require_keys_eq!(maybe_singer.address, ctx.accounts.singer.key());
 
         let clock = Clock::get()?;
-        // TODO: collection의 owner가 server인 경우에만 이벤트 등록 가능
-        // TODO: collection의 creator 중 하나가 server (verified)인 경우에만 이벤트 등록 가능
-        // TODO: collection의 creator 중 하나인 경우에만 이벤트 등록 가능
+        require_gt!(end_timestamp, clock.unix_timestamp);
+
+        let event = &mut ctx.accounts.event;
+        event.authority = ctx.accounts.creator.key();
+        event.singer = ctx.accounts.singer.key();
+        event.start_timestamp = start_timestamp;
+        event.end_timestamp = end_timestamp;
+        event.winners_total = winners_total;
+        event.winners = Vec::new();
+        event.singer_name = singer_name;
+        event.name = name;
+        event.description = description;
+        event.goods = goods;
+        event.uri = uri;
         Ok(())
     }
 
     pub fn entry_event(ctx: Context<EntryEvent>) -> Result<()> {
-        // TODO: token의 owner인 경우에만 응모 가능
-        // TODO: token의 creators 중 하나가 server (verified)인 경우에만 응모 가능
-        // TODO: token의 creators 중 하나가 event의 creator와 일치하는 경우에만 응모 가능
-        // TODO: token의 uses의 remaining이 1 이상인 경우에만 응모 가능
-        // TODO: token의 uses의 remaining을 0으로 set
-        // TODO: token에 대응하는 유일한 Pda 생성
+        let event = &ctx.accounts.event;
+
+        let clock = Clock::get()?;
+        let now = clock.unix_timestamp;
+        require_gt!(now, event.start_timestamp);
+        require_gt!(event.end_timestamp, now);
+
+        let metadata = Metadata::try_from(&ctx.accounts.metadata_pda)?;
+        let creators = metadata.creators.unwrap();
+        let maybe_server = creators.get(0).unwrap();
+        let maybe_singer = creators.get(2).unwrap();
+        require_eq!(maybe_server.verified, true);
+        require_keys_eq!(maybe_server.address, SERVER_PUBKEY);
+        require_keys_eq!(maybe_singer.address, event.singer);
+
+        let uses = metadata.uses.unwrap();
+        require_gte!(uses.remaining, 1);
+
+        let participant = &ctx.accounts.participant;
+        UseV1CpiBuilder::new(&ctx.accounts.metadata_program)
+            .authority(&participant)
+            .mint(&ctx.accounts.mint.to_account_info())
+            .metadata(&ctx.accounts.metadata_pda)
+            .payer(&participant)
+            .system_program(&ctx.accounts.system_program)
+            .sysvar_instructions(&ctx.accounts.sysvar_instructions)
+            .invoke()?;
+
+        let entry = &mut ctx.accounts.entry;
+        entry.event = event.key();
+        entry.participant = participant.key();
         Ok(())
     }
 
-    pub fn pick_winner(ctx: Context<PickWinner>) -> Result<()> {
-        // TODO: event의 authority인 경우에만 당첨자 선정 가능
-        // TODO: event의 ended_timestamp 이후에만 당첨자 선정 가능
-        // TODO: event의 winners_remaining이 1 이상인 경우에만 당첨자 선정 가능
-        // TODO: event의 remaining을 1 낮추고 무작위 뽑기
-        // TODO: 뽑힌 당첨자의 Entry의 is_winner를 true로 set
+    pub fn pick_winner(ctx: Context<PickWinner>, participants: Vec<Pubkey>) -> Result<()> {
+        let event = &mut ctx.accounts.event;
+
+        let recent_slothashes = &ctx.accounts.recent_slothashes.to_account_info();
+        let data = recent_slothashes.data.borrow();
+        let most_recent = array_ref![data, 12, 8];
+
+        let clock = Clock::get()?;
+        let seed = u64::from_le_bytes(*most_recent).saturating_sub(clock.unix_timestamp as u64);
+
+        let remainder = seed as usize % (participants.len());
+        let winner = participants.get(remainder).unwrap();
+
+        event.winners.push(*winner);
         Ok(())
     }
 }
@@ -56,41 +111,46 @@ pub mod event {
 #[account]
 pub struct Event {
     pub authority: Pubkey,
-    pub creator: Pubkey,
-    pub started_timestamp: i64,
-    pub ended_timestamp: i64,
-    pub winners_remaining: u16,
+    pub singer: Pubkey,
+    pub start_timestamp: i64,
+    pub end_timestamp: i64,
     pub winners_total: u16,
+    pub winners: Vec<Pubkey>,
 
+    pub singer_name: String,
     pub name: String,
     pub description: String,
-    pub uri: String,
-    pub singer: String,
     pub goods: String,
+    pub uri: String,
 }
 
 #[account]
 pub struct Entry {
     pub event: Pubkey,
     pub participant: Pubkey,
-    pub is_winner: bool,
 }
 
 #[derive(Accounts)]
 pub struct CreateEvent<'info> {
     #[account(mut)]
     pub creator: Signer<'info>,
+    /// CHECK:
+    pub singer: AccountInfo<'info>,
 
     #[account(
         init,
         payer = creator,
         space = 8
+            + 32 + 32 + 8 + 8 + 2 + (4 + 32 * 100)
             + (4 + 20) + (4 + 40) + (4 + 256) + (4 + 40) + (4 + 20)
-            + 32 + 32 + 2 + 2 + 8 + 8
     )]
     pub event: Account<'info, Event>,
 
-    pub clock: Sysvar<'info, Clock>,
+    #[account(owner = SERVER_PUBKEY)]
+    pub collection_token: Account<'info, TokenAccount>,
+    /// CHECK:
+    pub collection_metadata_pda: AccountInfo<'info>,
+
     pub system_program: Program<'info, System>,
 }
 
@@ -98,13 +158,42 @@ pub struct CreateEvent<'info> {
 pub struct EntryEvent<'info> {
     #[account(mut)]
     pub participant: Signer<'info>,
+
+    pub event: Account<'info, Event>,
+    #[account(
+        init,
+        payer = participant,
+        space = 72
+    )]
+    pub entry: Account<'info, Entry>,
+
+    pub mint: Account<'info, Mint>,
+    #[account(mut, owner = participant.key())]
+    pub token: Account<'info, TokenAccount>,
+    /// CHECK:
+    #[account(mut)]
+    pub metadata_pda: AccountInfo<'info>,
+
+    /// CHECK:
+    #[account(address = mpl_token_metadata::ID)]
+    pub metadata_program: AccountInfo<'info>,
+    /// CHECK:
+    #[account(address = Instructions::id())]
+    pub sysvar_instructions: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
 pub struct PickWinner<'info> {
-    #[account(mut)]
+    #[account(mut, address = event.authority)]
     pub creator: Signer<'info>,
 
-    #[account(mut)]
+    #[account(
+        mut,
+        constraint = event.end_timestamp < Clock::get()?.unix_timestamp,
+        constraint = event.winners.len() < event.winners_total as usize
+    )]
     pub event: Account<'info, Event>,
+
+    pub recent_slothashes: Sysvar<'info, SlotHashes>,
 }
--- blockchain/tests/event.ts
+++ blockchain/tests/event.ts
@@ -1,7 +1,11 @@
 import * as anchor from '@coral-xyz/anchor'
+import NodeWallet from '@coral-xyz/anchor/dist/cjs/nodewallet'
 import { Event } from '../target/types/event'
 import { expect } from 'chai'
-import NodeWallet from '@coral-xyz/anchor/dist/cjs/nodewallet'
+import * as splToken from '@solana/spl-token'
+import { Metaplex } from '@metaplex-foundation/js'
+import { SYSVAR_INSTRUCTIONS_PUBKEY } from '@solana/web3.js'
+import { MPL_TOKEN_METADATA_PROGRAM_ID } from '@metaplex-foundation/mpl-token-metadata'
 
 describe('market', async () => {
   const provider = anchor.AnchorProvider.env()
@@ -9,17 +13,67 @@ describe('market', async () => {
   const program = anchor.workspace.Market as anchor.Program<Event>
 
   const server = (provider.wallet as NodeWallet).payer
+  const creator = anchor.web3.Keypair.generate()
+  const singer = anchor.web3.Keypair.generate()
+  const participant = anchor.web3.Keypair.generate()
+
+  const event = anchor.web3.Keypair.generate()
+  const entry = anchor.web3.Keypair.generate()
+
+  const collectionMint = anchor.web3.Keypair.generate().publicKey
+  const mint = anchor.web3.Keypair.generate().publicKey
+  let collectionToken: anchor.web3.PublicKey;
+  let collectionMetadataPda: anchor.web3.PublicKey;
+  let token: anchor.web3.PublicKey;
+  let metadataPda: anchor.web3.PublicKey;
+
+  before(async () => {
+    await provider.connection.requestAirdrop(creator.publicKey, 1 * anchor.web3.LAMPORTS_PER_SOL)
+    await provider.connection.requestAirdrop(participant.publicKey, 1 * anchor.web3.LAMPORTS_PER_SOL)
+
+    collectionToken = splToken.getAssociatedTokenAddressSync(
+      collectionMint,
+      server.publicKey,
+    )
+    token = splToken.getAssociatedTokenAddressSync(
+      mint,
+      participant.publicKey,
+    )
+
+    const metaplex = Metaplex.make(provider.connection)
+    collectionMetadataPda = metaplex
+      .nfts()
+      .pdas()
+      .metadata({ mint: collectionMint })
+    metadataPda = metaplex
+      .nfts()
+      .pdas()
+      .metadata({ mint })
+  })
 
   it('Create Event', async () => {
+    const start = new Date()
+    const end = new Date()
+    end.setDate(end.getDate() + 3)
+
     const tx = program.methods.createEvent(
+      new anchor.BN(start.getTime()),
+      new anchor.BN(end.getTime()),
+      1,
+      'singer_name',
       'name',
       'description',
-      'uri',
-      'signer',
       'goods',
+      'uri',
     )
-      .accounts({})
-      .signers([])
+      .accounts({
+        creator: creator.publicKey,
+        singer: singer.publicKey,
+        event: event.publicKey,
+        collectionToken,
+        collectionMetadataPda,
+      })
+      .signers([creator, event])
       .rpc()
     console.log('TxHash ::', tx)
 
@@ -29,8 +83,17 @@ describe('market', async () => {
 
   it('Entry Event', async () => {
     const tx = program.methods.entryEvent()
-      .accounts({})
-      .signers([])
+      .accounts({
+        participant: participant.publicKey,
+        event: event.publicKey,
+        entry: entry.publicKey,
+        mint,
+        token,
+        metadataPda,
+        metadataProgram: MPL_TOKEN_METADATA_PROGRAM_ID,
+        sysvarInstructions: SYSVAR_INSTRUCTIONS_PUBKEY,
+      })
+      .signers([participant, entry])
       .rpc()
     console.log('TxHash ::', tx)
 
@@ -39,13 +102,18 @@ describe('market', async () => {
   })
 
   it('Pick Winner', async () => {
-    const tx = program.methods.pickWinner()
-      .accounts({})
-      .signers([])
+    const participants = [participant.publicKey]
+
+    const tx = program.methods.pickWinner(participants)
+      .accounts({
+        creator: creator.publicKey,
+        event: event.publicKey,
+      })
+      .signers([creator])
       .rpc()
     console.log('TxHash ::', tx)
 
-    const [entry] = await program.account.entry.all()
-    expect(entry.account.isWinner).to.eq(1)
+    const eventState = await program.account.event.fetch(event.publicKey)
+    expect(eventState.winners.length).to.eq(0)
   })
 })
