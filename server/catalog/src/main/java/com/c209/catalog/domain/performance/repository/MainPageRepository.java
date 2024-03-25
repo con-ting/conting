cedf20298e6760cea6abf64fccea24db099ca518
오유진 <yjin0414@naver.com>
2024-03-25 10:12:37 +0900

    [BE]Refactor: 오타 수정

--- server/catalog/src/main/java/com/c209/catalog/domain/performance/repository/MainPageRepository.java
+++ server/catalog/src/main/java/com/c209/catalog/domain/performance/repository/MainPageRepository.java
@@ -15,8 +15,8 @@ import java.util.Optional;
 @Repository
 public interface MainPageRepository extends JpaRepository<Performance, Long> {
     @Query("SELECT new com.c209.catalog.domain.performance.dto.info.PShowInfo( " +
-            "p.id, p.posterImage, p.title, p.hall.id, p.hall.name, p.reservationType, p.reservationStartDatetime, p.reservationEndDatetime, p.startDate, p.endDate, p.viewCount) " +
-            "FROM Performance p WHERE p.endDate > NOW() ORDER BY p.viewCount DESC LIMIT 10")
+            "p.id, p.posterImage, p.title, p.hall.id, p.hall.name, p.reservationType, p.reservationStartDatetime, p.reservationEndDatetime, p.startDate, p.endDate, p.view) " +
+            "FROM Performance p WHERE p.endDate > NOW() ORDER BY p.view DESC LIMIT 10")
     Optional<List<PShowInfo>> getPShowsList();
 
     @Query("SELECT new com.c209.catalog.domain.performance.dto.info.FShowInfo( " +
