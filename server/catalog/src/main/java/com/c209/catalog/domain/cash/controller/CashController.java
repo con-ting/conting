2ae68a22388cfdcade183e3d9d16a5280a24eaf5
김한주 <the_1_week@naver.com>
2024-03-31 00:47:47 +0900

    [BE-Catalog] Hotfix : 캐시 api 토큰 안타게끔 변경

--- server/catalog/src/main/java/com/c209/catalog/domain/cash/controller/CashController.java
+++ server/catalog/src/main/java/com/c209/catalog/domain/cash/controller/CashController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("catalog/cash")
+@RequestMapping("cache")
 @RequiredArgsConstructor
 public class CashController {
     private final CashService cashService;
