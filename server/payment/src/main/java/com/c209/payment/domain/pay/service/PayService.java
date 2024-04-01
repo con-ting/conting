b7ac1104e53f3cfe491a4661f17199b363e8346d
김한주 <the_1_week@naver.com>
2024-04-01 11:59:24 +0900

    [BE-Pay] Hotfix: 종속성 추가

--- server/payment/src/main/java/com/c209/payment/domain/pay/service/PayService.java
+++ server/payment/src/main/java/com/c209/payment/domain/pay/service/PayService.java
@@ -1,6 +1,8 @@
 package com.c209.payment.domain.pay.service;
 
 import com.c209.payment.domain.pay.dto.request.PayAuthRequest;
+import com.c209.payment.domain.order.dto.request.OrderSuccessRequest;
+
 import reactor.core.publisher.Mono;
 
 public interface PayService {
