a69b4649327ace73a2e90aa34a2f8352b2a649be
Younghun Ko <koyo@koyo.kr>
2024-04-02 05:26:19 +0900

    [FE-Web3] Chore : metaplex-foundation/js를 사용하지 않고 metadata 가져오는 코드 작성 (#83)

--- blockchain/tests/did.ts
+++ blockchain/tests/did.ts
@@ -1,55 +1,39 @@
 import * as anchor from '@coral-xyz/anchor'
-import { Did } from '../target/types/did'
-import { expect } from 'chai'
-import * as splToken from '@solana/spl-token'
-import { Metaplex } from '@metaplex-foundation/js'
-import { SYSVAR_INSTRUCTIONS_PUBKEY, SYSVAR_SLOT_HASHES_PUBKEY } from '@solana/web3.js'
-import { MPL_TOKEN_METADATA_PROGRAM_ID } from '@metaplex-foundation/mpl-token-metadata'
 import NodeWallet from '@coral-xyz/anchor/dist/cjs/nodewallet'
-import {
-  agency, singer, participant1 as par1, participant2 as par2, participant3 as par3,
-  collectionMint, par1sMint, par2sMint, par3sMint
-} from './env'
+import * as web3 from '@solana/web3.js'
+import { expect } from 'chai'
 
-describe('did', async () => {
+import { Did } from '../target/types/did'
+import { participant1 as par1, participant2 as par2, participant3 as par3 } from './env'
+
+describe('did', () => {
   const provider = anchor.AnchorProvider.env()
   anchor.setProvider(provider)
   const program = anchor.workspace.Did as anchor.Program<Did>
   const server = (provider.wallet as NodeWallet).payer
 
-  let family12: anchor.web3.PublicKey;
-  let family23: anchor.web3.PublicKey;
+  let family12: web3.PublicKey
+  let family23: web3.PublicKey
   before(async () => {
-    await provider.connection.requestAirdrop(par1.publicKey, anchor.web3.LAMPORTS_PER_SOL)
-    await provider.connection.requestAirdrop(par2.publicKey, anchor.web3.LAMPORTS_PER_SOL)
-    await provider.connection.requestAirdrop(par3.publicKey, anchor.web3.LAMPORTS_PER_SOL)
+    await provider.connection.requestAirdrop(par1.publicKey, web3.LAMPORTS_PER_SOL)
+    await provider.connection.requestAirdrop(par2.publicKey, web3.LAMPORTS_PER_SOL)
+    await provider.connection.requestAirdrop(par3.publicKey, web3.LAMPORTS_PER_SOL)
 
-    family12 = anchor.web3.PublicKey.findProgramAddressSync(
-      [
-        anchor.utils.bytes.utf8.encode('family'),
-        par1.publicKey.toBuffer(),
-        par2.publicKey.toBuffer(),
-      ],
-      program.programId
+    family12 = web3.PublicKey.findProgramAddressSync(
+      [anchor.utils.bytes.utf8.encode('family'), par1.publicKey.toBuffer(), par2.publicKey.toBuffer()],
+      program.programId,
     )[0]
-    family23 = anchor.web3.PublicKey.findProgramAddressSync(
-      [
-        anchor.utils.bytes.utf8.encode('family'),
-        par2.publicKey.toBuffer(),
-        par3.publicKey.toBuffer(),
-      ],
-      program.programId
+    family23 = web3.PublicKey.findProgramAddressSync(
+      [anchor.utils.bytes.utf8.encode('family'), par2.publicKey.toBuffer(), par3.publicKey.toBuffer()],
+      program.programId,
     )[0]
 
     await new Promise((resolve) => setTimeout(resolve, 1000))
   })
 
   it('Issue Cert 1+2', async () => {
-
-    const tx = await program.methods.issueCert(
-      new anchor.BN(1),
-      new anchor.BN(2),
-    )
+    const tx = await program.methods
+      .issueCert(new anchor.BN(1), new anchor.BN(2))
       .accounts({
         server: server.publicKey,
         family: family12,
@@ -65,10 +49,8 @@ describe('did', async () => {
   })
 
   it('Issue Cert 2+3', async () => {
-    const tx = await program.methods.issueCert(
-      new anchor.BN(2),
-      new anchor.BN(3),
-    )
+    const tx = await program.methods
+      .issueCert(new anchor.BN(2), new anchor.BN(3))
       .accounts({
         server: server.publicKey,
         family: family23,
@@ -84,7 +66,8 @@ describe('did', async () => {
   })
 
   it('Revoke Cert Lower', async () => {
-    const tx = await program.methods.revokeCertLower()
+    const tx = await program.methods
+      .revokeCertLower()
       .accounts({
         lower: par1.publicKey,
         family: family12,
@@ -99,7 +82,8 @@ describe('did', async () => {
   })
 
   it('Revoke Cert Upper', async () => {
-    const tx = await program.methods.revokeCertUpper()
+    const tx = await program.methods
+      .revokeCertUpper()
       .accounts({
         upper: par3.publicKey,
         family: family23,
