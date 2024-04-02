304783144ab96abd153672156f5a1daad46465eb
김형민 <kimtuti@naver.com>
2024-04-02 18:01:37 +0900

    [FE-Page] Add : 응모권 양도 API 로직 구현 (#78)

--- app/frontend/src/api/web3/did.ts
+++ app/frontend/src/api/web3/did.ts
@@ -1,61 +1,15 @@
-import {PublicKey} from '@solana/web3.js';
-import {AnchorProvider, Program} from '@coral-xyz/anchor';
-import {DID_PROGRAM_ID} from '../../config/web3Config.tsx';
-import {Did, IDL} from '../../config/web3Types/did.ts';
-import {findFamilyInfo} from './wallet.ts';
-
-export async function getFamilies(
-  myAddress: PublicKey,
-  connection: any,
-  anchorWallet: any,
-) {
-  const families = [];
-  const provider = new AnchorProvider(connection, anchorWallet, {
-    preflightCommitment: 'confirmed',
-    commitment: 'processed',
-  });
-  const program = new Program<Did>(IDL, DID_PROGRAM_ID, provider);
-
-  const familiesLowerIsMe = await program.account.family.all([
-    {
-      memcmp: {
-        offset: 8,
-        bytes: myAddress.toBase58(),
-      },
-    },
-  ]);
-  for (const familiesLowerIsMeElement of familiesLowerIsMe) {
-    const data = await findFamilyInfo(
-      String(familiesLowerIsMeElement.account.upper),
-    );
-    families.push({
-      id: familiesLowerIsMeElement.account.upperId,
-      email: data.email,
-      name: data.name,
-      wallet: String(familiesLowerIsMeElement.account.upper),
-    });
-  }
-
-  const familiesUpperIsMe = await program.account.family.all([
-    {
-      memcmp: {
-        offset: 8 + 32,
-        bytes: myAddress.toBase58(),
-      },
-    },
-  ]);
-
-  for (const familiesUpperIsMeElement of familiesUpperIsMe) {
-    const data = await findFamilyInfo(
-      String(familiesUpperIsMeElement.account.lower),
-    );
-    families.push({
-      id: familiesUpperIsMeElement.account.lowerId,
-      email: data.email,
-      name: data.name,
-      wallet: String(familiesUpperIsMeElement.account.lower),
-    });
-  }
-  console.log('[getFamiliesApi response] = ', families);
-  return families;
-}
+import tokenInstance from '../../utils/axios/axiosTokenInstance';
+import {familyType} from './web3.ts';
+const didUrl: string = 'did';
+export const findFamilyInfo = async (params: {
+  showid: number; // 공연 id
+  owner_fingerprint_key: string; // 지문 인증 공개키
+  owner_id: number; // 내 user_id
+  owner_wallet: string; // 내 지갑 주소
+  families: Array<familyType>;
+}) => {
+  console.log('[findFamilyInfo request] = ', params);
+  const res = await tokenInstance.get(`${didUrl}/transfer/v2`, params);
+  console.log('[findFamilyInfo response] = ', res.data);
+  return res.data;
+};
---
+++ app/frontend/src/api/web3/web3.ts
@@ -0,0 +1,66 @@
+import {PublicKey} from '@solana/web3.js';
+import {AnchorProvider, Program} from '@coral-xyz/anchor';
+import {DID_PROGRAM_ID} from '../../config/web3Config.tsx';
+import {Did, IDL} from '../../config/web3Types/did.ts';
+import {findFamilyInfo} from './wallet.ts';
+export type familyType = {
+  id: number;
+  email: string;
+  name: string;
+  wallet: string;
+};
+export async function getFamilies(
+  myAddress: PublicKey,
+  connection: any,
+  anchorWallet: any,
+) {
+  const families = [];
+  const provider = new AnchorProvider(connection, anchorWallet, {
+    preflightCommitment: 'confirmed',
+    commitment: 'processed',
+  });
+  const program = new Program<Did>(IDL, DID_PROGRAM_ID, provider);
+
+  const familiesLowerIsMe = await program.account.family.all([
+    {
+      memcmp: {
+        offset: 8,
+        bytes: myAddress.toBase58(),
+      },
+    },
+  ]);
+  for (const familiesLowerIsMeElement of familiesLowerIsMe) {
+    const data = await findFamilyInfo(
+      String(familiesLowerIsMeElement.account.upper),
+    );
+    families.push({
+      id: familiesLowerIsMeElement.account.upperId.toNumber(),
+      email: data.email,
+      name: data.name,
+      wallet: String(familiesLowerIsMeElement.account.upper),
+    });
+  }
+
+  const familiesUpperIsMe = await program.account.family.all([
+    {
+      memcmp: {
+        offset: 8 + 32,
+        bytes: myAddress.toBase58(),
+      },
+    },
+  ]);
+
+  for (const familiesUpperIsMeElement of familiesUpperIsMe) {
+    const data = await findFamilyInfo(
+      String(familiesUpperIsMeElement.account.lower),
+    );
+    families.push({
+      id: familiesUpperIsMeElement.account.lowerId.toNumber(),
+      email: data.email,
+      name: data.name,
+      wallet: String(familiesUpperIsMeElement.account.lower),
+    });
+  }
+  console.log('[getFamiliesApi response] = ', families);
+  return families;
+}
