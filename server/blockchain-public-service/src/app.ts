2dcdf8394be63df49212b56d0162d95d90696f55
Younghun Ko <koyo@koyo.kr>
2024-04-04 03:07:19 +0900

    [BE-Web3] Improve : confirmed 시간을 고려하여 반환값이 없을시  재시도 (#88)

--- server/blockchain-public-service/src/app.ts
+++ server/blockchain-public-service/src/app.ts
@@ -1,6 +1,6 @@
 import { updateV1, usesToggle } from '@metaplex-foundation/mpl-token-metadata'
 import { publicKey } from '@metaplex-foundation/umi'
-import { PartiallyDecodedInstruction, PublicKey } from '@solana/web3.js'
+import { ParsedTransactionWithMeta, PartiallyDecodedInstruction, PublicKey } from '@solana/web3.js'
 import Fastify from 'fastify'
 
 import { getPort, getSecret, initConnection, initUmi } from './init.js'
@@ -14,18 +14,29 @@ const port = getPort()
 const EVENT_PROGRAM_ID = new PublicKey('Even2kqboEgiEv8ozq4fMyiDi727VeRbTD7SQogF5vrn')
 
 fastify.post<{ Params: { tx: string } }>('/use/:tx', async (request, reply) => {
-  const transaction = await connection.getParsedTransaction(request.params.tx)
-  const instruction = transaction.transaction.message.instructions[0] as PartiallyDecodedInstruction
-  const programId = instruction.programId
-  if (!EVENT_PROGRAM_ID.equals(programId)) {
-    throw new Error(`'${EVENT_PROGRAM_ID}'에서 생성된 Transaction이 아닙니다.`)
+  const maxRetry = 5
+  let tx: ParsedTransactionWithMeta | null
+  for (let i = 0; i < maxRetry; i++) {
+    tx = await connection.getParsedTransaction(request.params.tx)
+    if (tx !== null) {
+      break
+    }
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+  }
+  if (tx === null) {
+    throw new Error('트랜잭션을 찾을 수 없습니다.')
+  }
+  const instruction = tx.transaction.message.instructions[0] as PartiallyDecodedInstruction
+
+  if (!EVENT_PROGRAM_ID.equals(instruction.programId)) {
+    throw new Error(`'${EVENT_PROGRAM_ID}'에서 생성된 트랜잭션이 아닙니다.`)
   }
   const mint = instruction.accounts[3]
-  const { signature: tx } = await updateV1(umi, {
+  const { signature } = await updateV1(umi, {
     mint: publicKey(mint),
     uses: usesToggle('Clear'),
   }).sendAndConfirm(umi)
-  return { tx }
+  return { tx: signature }
 })
 
 const start = async () => {
