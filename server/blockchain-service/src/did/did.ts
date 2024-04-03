0569822d38fc4a37b6b27eda67dc120e25d460c9
Younghun Ko <koyo@koyo.kr>
2024-04-04 06:05:59 +0900

    [BE-Web3] Hotfix : 프로젝트 설정, API 일반화 및 API 버그 수정 (#90)

--- server/blockchain-service/src/did/did.ts
+++ server/blockchain-service/src/did/did.ts
@@ -7,6 +7,11 @@ import { DidInput } from './types.js'
 
 const DID_PROGRAM_ID = 'DiDiDgTdcYhe7jemETo4u5B6GNtsv1BPDnRHBQJtVoEj'
 
+export const getFamilyList = async (provider: AnchorProvider) => {
+  const program = new Program<Did>(IDL, DID_PROGRAM_ID, provider)
+  return await program.account.family.all()
+}
+
 export const issueFamily = async (
   provider: AnchorProvider,
   input: DidInput,
@@ -33,21 +38,37 @@ export const issueFamily = async (
 /**
  * 테스트 목적으로 생성한 함수입니다.
  */
-export const revokeFamilyByLower = async (
+export const revokeFamily = async (
   provider: AnchorProvider,
   family: PublicKey,
-  lower: PublicKey,
   secret: Keypair,
 ) => {
   const program = new Program<Did>(IDL, DID_PROGRAM_ID, provider)
-  const tx = await program.methods
-    .revokeCertLower()
-    .accounts({
-      lower,
-      server: program.provider.publicKey,
-      family,
-    })
-    .signers([secret])
-    .rpc()
+  const familyInfo = await program.account.family.fetch(family)
+  const lower_or_upper = secret.publicKey
+  let tx: string
+  if (lower_or_upper.equals(familyInfo.lower)) {
+    tx = await program.methods
+      .revokeCertLower()
+      .accounts({
+        lower: lower_or_upper,
+        server: program.provider.publicKey,
+        family,
+      })
+      .signers([secret])
+      .rpc()
+  } else if (lower_or_upper.equals(familyInfo.upper)) {
+    tx = await program.methods
+      .revokeCertUpper()
+      .accounts({
+        upper: lower_or_upper,
+        server: program.provider.publicKey,
+        family,
+      })
+      .signers([secret])
+      .rpc()
+  } else {
+    throw new Error('lower 또는 upper 중 하나가 아닙니다.')
+  }
   return tx
 }
