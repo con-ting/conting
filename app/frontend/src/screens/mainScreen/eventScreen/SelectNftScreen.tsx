f62ac85bcd7c30c335215932f939b44a93c7ef29
김형민 <kimtuti@naver.com>
2024-04-07 00:03:33 +0900

    [FE-Page] Add : nft 선택 모달 구현 (#78)

--- app/frontend/src/screens/mainScreen/eventScreen/SelectNftScreen.tsx
+++ app/frontend/src/screens/mainScreen/eventScreen/SelectNftScreen.tsx
@@ -1,21 +1,39 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {View, Animated, Dimensions, PanResponder} from 'react-native';
-import {PublicKey} from '@solana/web3.js';
-import {useRecoilState} from 'recoil';
-import {userInfoState} from '../../../utils/recoil/Atoms';
-import {useConnection} from '../../../components/mobileWalletAdapter/providers/ConnectionProvider';
-import NFTCard from '../../../components/card/NFTCard';
-import {MAINBLACK, MAINWHITE, MAINYELLOW} from '../../../config/Color.ts';
+import React, {useState, useRef, useEffect} from 'react';
+import {
+  View,
+  Text,
+  Animated,
+  PanResponder,
+  TouchableOpacity,
+  Alert,
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
+import {
+  CUTEYELLOW,
+  MAINBLACK,
+  MAINWHITE,
+  MAINYELLOW,
+} from '../../../config/Color.ts';
 import {nftListFindByMyWallet} from '../../../api/web3/nft.ts';
-import NftBack from '../../../components/nft/NftBack.tsx';
+import {PublicKey} from '@solana/web3.js';
+import {useConnection} from '../../../components/mobileWalletAdapter/providers/ConnectionProvider.tsx';
+import {useRecoilState} from 'recoil';
+import {userInfoState} from '../../../utils/recoil/Atoms.ts';
+import NftSelectCard from '../../../components/nft/NftSelectCard.tsx';
+import {MAINFONT} from '../../../config/Font.ts';
 
 const {width} = Dimensions.get('window');
-export default function SelectNftScreen({route}) {
+const CARD_WIDTH = width * 0.9;
+const CARD_HEIGHT = CARD_WIDTH * 0.56;
+const CARD_OFFSET = 15;
+
+export default function SelectNftScreen(props: {selectItem: any}) {
   const [cards, setCards] = useState([]);
-  const cardRef = useRef('fold'); // "fold" or "unfold"
-  const [focus, setFocus] = useState(5);
-  const yAnim = useRef(new Animated.Value(0)).current;
-  const rotateZAnim = useRef(new Animated.Value(0)).current;
+  const [removedCards, setRemovedCards] = useState([]);
+
+  const pan = useRef(new Animated.ValueXY()).current;
+
   const [userInfo, setUserInfo] = useRecoilState(userInfoState);
   const {connection} = useConnection();
 
@@ -25,168 +43,148 @@ export default function SelectNftScreen({route}) {
         connection: connection,
         myWalletAddress: new PublicKey(userInfo?.walletAddress),
       });
-      // Initialize each item with an Animated.Value
+      console.log('api 호출');
+      // Initialize each item with an Animated.Value and the nft data
       const initializedList = nftList.map((item, index) => ({
         ...item,
-        xAnim: new Animated.Value(0), // Here we add an Animated.Value for each item
+        xAnim: new Animated.Value(0),
       }));
       setCards(initializedList);
     };
     settingList();
-  }, []);
-
-  // panResponder와 관련된 코드는 생략하고, 필요한 수정을 진행해주세요.
-  const panResponer = PanResponder.create({
-    onMoveShouldSetPanResponder: () => true,
-    onPanResponderMove: (evt, gestureState) => {
-      const {dy, dx} = gestureState;
-      // dy, dx 뭐가 더 크게 변했을까
-      const XSlider = Math.abs(dy) < Math.abs(dx);
-      const YSlider = Math.abs(dx) < Math.abs(dy);
-      if (XSlider) {
-        // 카드 버리기
-        if (dx < -5 && cardRef.current === 'fold' && 0 <= focus) {
-          cards[focus].xAnim.setValue(dx);
-        }
-
-        // 카드 가져오기
-        // if (5 < dx && cardRef.current === 'fold' && focus <= 5) {
-        //   card[focus].xAnim.setValue(dx);
-        // }
-      }
-
-      if (YSlider) {
-        if (5 < dy && dy < 100 && cardRef.current === 'fold') {
-          yAnim.setValue(dy);
-        }
-
-        if (5 < dy && dy < 100 && cardRef.current === 'unfold') {
-          rotateZAnim.setValue(dy);
-        }
-
-        if (-75 < dy && dy < 5 && cardRef.current === 'unfold') {
-          yAnim.setValue(65 + dy);
-        }
-      }
-    },
-    onPanResponderEnd: (evt, gestureState) => {
-      const {dy, dx} = gestureState;
-      const XSlider = Math.abs(dy) < Math.abs(dx);
-      const YSlider = Math.abs(dx) < Math.abs(dy);
-      if (XSlider) {
-        if (dx < -5 && cardRef.current === 'fold' && 0 <= focus) {
-          Animated.timing(cards[focus].xAnim, {
-            toValue: -600,
+  }, [connection, userInfo?.walletAddress]);
+  // useEffect(() => {
+  //   console.log('cards log = ', cards);
+  // }, [cards]);
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: Animated.event([null, {dx: pan.x}], {
+        useNativeDriver: false,
+      }),
+      onPanResponderRelease: (e, {dx}) => {
+        if (dx < -100) {
+          Animated.timing(pan, {
+            toValue: {x: -width, y: 0},
             duration: 300,
             useNativeDriver: false,
-          }).start(({finished}) => {
-            if (finished) {
-              setFocus(el => el - 1);
-            }
+          }).start(() => {
+            setCards(currentCards => {
+              const newCards = [...currentCards];
+              const topCard = newCards.pop(); // 배열의 마지막 요소를 제거
+              if (topCard) {
+                newCards.unshift(topCard); // 배열의 시작에 추가
+              }
+              pan.setValue({x: 0, y: 0});
+              return newCards; // 새로운 배열 상태 반환
+            });
           });
-        }
-
-        if (5 < dx && cardRef.current === 'fold' && focus < 5) {
-          Animated.timing(cards[focus + 1].xAnim, {
-            toValue: 0,
-            duration: 300,
-            useNativeDriver: false,
-          }).start(({finished}) => {
-            if (finished) {
-              setFocus(el => el + 1);
-            }
-          });
-        }
-      }
-
-      if (YSlider) {
-        if (5 < dy) {
-          Animated.spring(yAnim, {
-            toValue: 65,
-            duration: 300,
-            useNativeDriver: false,
-          }).start();
-          cardRef.current = 'unfold';
-        }
-
-        if (5 < dy && cardRef.current === 'unfold') {
-          Animated.spring(rotateZAnim, {
-            toValue: 0,
-            duration: 300,
+        } else {
+          Animated.spring(pan, {
+            toValue: {x: 0, y: 0},
             useNativeDriver: false,
           }).start();
         }
+      },
+    }),
+  ).current;
 
-        if (dy < -5) {
-          Animated.spring(yAnim, {
-            toValue: 0,
-            duration: 300,
-            useNativeDriver: false,
-          }).start();
-          cardRef.current = 'fold';
-        }
-      }
-    },
-  });
-  return (
-    <View
-      {...panResponer.panHandlers}
-      style={{
-        flex: 1,
-        borderWidth: 10,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <View
-        style={{
-          position: 'relative',
-          width: width * 0.7,
-          height: width * 0.7 * 0.58 + (cards.length - 1) * 20,
-        }}>
-        {cards.map((card, index) => {
-          const translateY = yAnim.interpolate({
-            inputRange: [0, 100],
-            outputRange: [0, index * 20], // 예를 들어, 각 카드가 조금씩 아래로 내려가도록 설정
-          });
-          return (
-            <Animated.View
-              key={index}
-              style={{
-                transform: [
-                  {translateY},
-                  {translateX: card.xAnim},
-                  {
-                    rotateZ: rotateZAnim.interpolate({
-                      inputRange: [0, 20],
-                      outputRange: ['0deg', '2deg'],
-                    }),
-                  },
-                ],
-                position: 'absolute',
-                marginTop: index * 20,
+  const renderCards = () => {
+    return (
+      <View style={styles.cardsContainer}>
+        {cards.map((card, index) => (
+          <Animated.View
+            key={`card_${index}`}
+            style={[
+              styles.card,
+              {
                 backgroundColor: MAINBLACK,
-                borderRadius: 4,
-                width: width * 0.7,
-                height: width * 0.7 * 0.58,
-                shadowOffset: {width: -3, height: -3},
-                shadowOpacity: 0.2,
-                shadowRadius: 10,
-              }}>
-              <NftBack
-                title={card.title}
-                ticketAddress={card.ticketAddress}
-                webmUrl={card.webmUrl.uri}
-                sellerFeeBasisPoints={card.sellerFeeBasisPoints}
-                primarySaleHappened={card.primarySaleHappened}
-                creatorAddressList={card.creatorAddressList}
-                jsonMetaData={card.jsonMetaData}
-                ownerAddress={card.ownerAddress}
-                accountData={card.accountData}
-              />
-            </Animated.View>
-          );
-        })}
+                transform: [
+                  {translateX: index === cards.length - 1 ? pan.x : 0},
+                ],
+                bottom: (cards.length - index - 1) * CARD_OFFSET,
+                borderWidth: index === cards.length - 1 ? 2 : 0,
+                borderColor: MAINYELLOW,
+              },
+            ]}
+            {...(index === cards.length - 1 ? panResponder.panHandlers : {})}>
+            <NftSelectCard
+              title={card.title}
+              ticketAddress={card.ticketAddress}
+              poster={card.poster.uri}
+              sellerFeeBasisPoints={card.sellerFeeBasisPoints}
+              primarySaleHappened={card.primarySaleHappened}
+              creatorAddressList={card.creatorAddressList}
+              jsonMetaData={card.jsonMetaData}
+              ownerAddress={card.ownerAddress}
+              accountData={card.accountData}
+            />
+          </Animated.View>
+        ))}
       </View>
+    );
+  };
+
+  const apiSendor = () => {
+    if (cards.length > 0) {
+      const topCard = cards[cards.length - 1];
+      Alert.alert(
+        'Top Card Info',
+        `이름: ${topCard.title}, 주소: ${topCard.ticketAddress}`,
+      );
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      {renderCards()}
+      <TouchableOpacity style={styles.button} onPress={apiSendor}>
+        <Text style={styles.buttonText}>응모권에 사용하기</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    paddingTop: 30,
+    paddingBottom: 30,
+    backgroundColor: CUTEYELLOW,
+  },
+  cardsContainer: {
+    width: CARD_WIDTH,
+    height: CARD_HEIGHT,
+    position: 'relative',
+  },
+  card: {
+    position: 'absolute',
+    width: CARD_WIDTH,
+    height: CARD_HEIGHT,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  text: {
+    fontSize: 22,
+    color: '#fff',
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: MAINYELLOW,
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontFamily: MAINFONT,
+    color: MAINBLACK,
+    fontSize: 16,
+  },
+});
